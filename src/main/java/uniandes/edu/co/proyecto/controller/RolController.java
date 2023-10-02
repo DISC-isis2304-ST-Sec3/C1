055ddc9e1227ab378b66ac87ad5ea9719d24cfd9
JuanRojasC-Andes <jd.rojasc12@uniandes.edu.co>
2023-10-01 21:39:08 -0500

    Controlador del rol con thymeleaf

--- src/main/java/uniandes/edu/co/proyecto/controller/RolController.java
+++ src/main/java/uniandes/edu/co/proyecto/controller/RolController.java
@@ -17,33 +17,36 @@ public class RolController {
 
     @GetMapping("/roles/{nombre}")
     public String getByID(@PathVariable("nombre") String nombre, Model model) {
-        model.addAttribute("rol", rolServicio.getByID(nombre));
-        return "rol";
+        model.addAttribute("message", rolServicio.getByID(nombre).getNombre());
+        return "roles";
     }
 
     @GetMapping("/roles/all")
     public String getAll(Model model) {
         List<Rol> results = rolServicio.getAll();
-        model.addAllAttributes(results);
-        return "all roles";
+        model.addAttribute("message", results.toString());
+        return "roles";
     }
 
     @PostMapping("/roles/create")
     public String create(@ModelAttribute Rol rol, Model model) {
         Rol rolSaved = rolServicio.save(rol);
-        return "rol saved";
+        model.addAttribute("message", rolSaved.getNombre());
+        return "roles";
     }
 
     @PostMapping("/roles/update")
     public String update(@ModelAttribute Rol rol, Model model) {
         Rol rolUpdated = rolServicio.update(rol);
-        return "rol updated";
+        model.addAttribute("message", rolUpdated.getNombre());
+        return "roles";
     }
 
     @DeleteMapping("/roles")
     public String delete(@ModelAttribute Rol rol, Model model) {
         Boolean deleted = rolServicio.deleteByID(rol.getNombre());
-        return "rol deleted";
+        model.addAttribute("message", String.format("%s rol deleted", rol.getNombre()));
+        return "roles";
     }
 
 }
